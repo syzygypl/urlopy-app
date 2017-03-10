905481645c22dbb5c4133a70110f940b36661a96
Mateusz Wit <mateusz.wit@syzygy.pl>
2017-03-10 17:59:31 +0100

    add firebase support

--- src/rootReducer/index.js
+++ src/rootReducer/index.js
@@ -1,1 +1,8 @@
+import { firebaseStateReducer } from 'react-redux-firebase';
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({
+  firebase: firebaseStateReducer,
+});
+
 export default state => state;
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,22 @@
 import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { reactReduxFirebase } from 'react-redux-firebase';
 
 import rootReducer from '../rootReducer';
 
 const middleware = [thunk];
+
+const config = {
+  apiKey: '<your-api-key>',
+  authDomain: '<your-auth-domain>',
+  databaseURL: '<your-database-url>',
+  storageBucket: '<your-storage-bucket>',
+};
+
+const createStoreWithFirebase = compose(
+  reactReduxFirebase(config, { userProfile: 'users' }),
+)(createStore);
 
 export default createStore(
   rootReducer,
