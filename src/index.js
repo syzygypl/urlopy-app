a7615d86b58ffaeabc8c23a94d8024ab337b76e9
Mateusz Wit <mateusz.wit@syzygy.pl>
2017-03-10 17:17:52 +0100

    add support for material-ui

--- src/index.js
+++ src/index.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import store from './store';
-
 import './index.css';
+import store from './store';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+
+injectTapEventPlugin();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <div>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/topics">Topics</Link></li>
-        </ul>
+    <MuiThemeProvider>
+      <Router>
+        <div>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+          </ul>
 
-        <hr />
+          <hr />
 
-        <Route exact path="/" component={() => (<div>Home</div>)} />
-        <Route path="/about" component={() => (<div>About</div>)} />
-        <Route path="/topics" component={() => (<div>Topics</div>)} />
-      </div>
-    </Router>
+          <Route exact path="/" component={() => <MyAwesomeReactComponent />} />
+          <Route path="/about" component={() => (<div>About</div>)} />
+          <Route path="/topics" component={() => (<div>Topics</div>)} />
+        </div>
+      </Router>
+    </MuiThemeProvider>
   </Provider>,
   document.getElementById('root'),
 );
