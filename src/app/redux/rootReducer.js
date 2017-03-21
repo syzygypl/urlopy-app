341ed790cf9fd2840a95b6026b4c2e60b29742df
KB <krinoid@users.noreply.github.com>
2017-03-21 14:43:31 +0100

    add redux-form reducer

--- src/app/redux/rootReducer.js
+++ src/app/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { firebaseStateReducer } from 'react-redux-firebase';
+import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
   firebase: firebaseStateReducer,
+  form: formReducer,
 });
 
 export default rootReducer;
