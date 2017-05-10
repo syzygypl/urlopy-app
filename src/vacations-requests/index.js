3f6ceaa71fdcb2ef08e302308d18107330c8b829
Mateusz Wit <mateusz.wit@syzygy.pl>
2017-05-10 12:23:44 +0200

    Remove add vac button and its functionality

--- src/vacations-requests/index.js
+++ src/vacations-requests/index.js
@@ -1,23 +1,10 @@
-import React, { PropTypes } from 'react';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
+import React from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import { firebaseConnect } from 'react-redux-firebase';
-
-import * as props from './props';
 
 import VacationsRequestsTable from './Vacations/VacationsRequestsTable';
 
-const VacationsRequests = ({ addVacationsRequest, firebase }) => (
+const VacationsRequests = () => (
   <Grid fluid>
-
-    <Row>
-
-      <button onClick={() => addVacationsRequest(firebase.auth().currentUser.uid)}>
-        add vac
-      </button>
-
-    </Row>
 
     <Row>
       <Col xs={12} md={12}>
@@ -28,41 +15,4 @@ const VacationsRequests = ({ addVacationsRequest, firebase }) => (
   </Grid>
 );
 
-VacationsRequests.defaultProps = {
-  vacationsRequests: {},
-};
-
-VacationsRequests.propTypes = {
-  firebase: props.firebase.isRequired,
-  addVacationsRequest: PropTypes.func.isRequired,
-};
-
-export default compose(
-  firebaseConnect(ownProps => (
-    [
-      `/vacationsRequests/${ownProps.currentUserID}`,
-    ]
-  )),
-  connect(
-    ({ currentUserID }) => ({ currentUserID }),
-    ((dispatch, ownProps) =>
-        ({
-          addVacationsRequest(ID) {
-            const addVacation = vacationsRequestID => ownProps.firebase.push(`/vacations/${vacationsRequestID}`, {
-              to: Date.now(),
-              from: Date.now(),
-              workDays: 4,
-            });
-
-            return ownProps.firebase
-              .push(`/vacationsRequests/${ID}`, {
-                notifyUser: 'Adam_Nowak',
-                vacationerID: ID,
-                status: 'accepted',
-              })
-              .then(newVacationsRequest => addVacation(newVacationsRequest.getKey()));
-          },
-        })
-    ),
-  ),
-)(VacationsRequests);
+export default VacationsRequests;
