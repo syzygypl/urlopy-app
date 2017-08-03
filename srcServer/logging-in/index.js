6c5b0591d207b04a2210ceab026db7192151eeca
Mateusz Wit <mateusz.wit@syzygy.pl>
2017-08-03 10:51:59 +0200

    move loginHandler out into a separate file

--- srcServer/logging-in/index.js
+++ srcServer/logging-in/index.js
@@ -1,35 +1,6 @@
 const router = require('express').Router();
 const ldapLib = require('ldapjs');
-
-const bindToLDAP = require('./bindToLDAP');
-const getFireToken = require('./getFireToken');
-const getEntryDataFromLDAP = require('./getEntryDataFromLDAP');
-
-const noAuth = (res, e) => res.status(401).send(e);
-
-const loginHandler = (ldap, req, res) => {
-  const ldapClient = ldap.createClient({
-    url: process.env.LDAP_URL,
-  });
-
-  const username = req.body.username;
-
-  const opts = {
-    filter: `(|(uid=${username})(mail=${username}))`,
-    scope: 'sub',
-  };
-
-  getEntryDataFromLDAP(ldapClient, opts)
-    .then(entry => bindToLDAP(ldapClient, entry.dn, req.body.password))
-    .catch(e => noAuth(res, e))
-    .then(ldapConnection =>
-      getFireToken(username)
-        .then(customToken => res.status(200).send(customToken))
-        .then(() => ldapConnection),
-    )
-    .then(({ client }) => client.unbind())
-    .catch(e => noAuth(res, e));
-};
+const loginHandler = require('./loginHandler');
 
 router.post('/login', loginHandler.bind(null, ldapLib));
 
