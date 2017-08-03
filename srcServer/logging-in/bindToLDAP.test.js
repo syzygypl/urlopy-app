5fcdce2b2cb5a43af2769b9b792292821b0b08c0
Mateusz Wit <mateusz.wit@syzygy.pl>
2017-08-03 10:43:25 +0200

    do some minor renaming fro bindToLDAP tests

--- srcServer/logging-in/bindToLDAP.test.js
+++ srcServer/logging-in/bindToLDAP.test.js
@@ -2,15 +2,15 @@ const bindToLDAP = require('./bindToLDAP');
 
 describe('bindToLDAP', () => {
   describe('when an error occured', () => {
-    const client = {
-      bind: (client_, b, cb) => {
+    const ldapClient = {
+      bind: (client, dn, cb) => {
         cb('some error');
       },
     };
 
     it('should have error message', async () => {
       try {
-        await bindToLDAP(client, 'dn', 'password');
+        await bindToLDAP(ldapClient, 'dn', 'password');
       } catch (e) {
         expect(e.err).toBe('some error');
       }
@@ -18,30 +18,30 @@ describe('bindToLDAP', () => {
 
     it('should return client', async () => {
       try {
-        await bindToLDAP(client, 'dn', 'password');
+        await bindToLDAP(ldapClient, 'dn', 'password');
       } catch (e) {
-        expect(e.client).toBe(client);
+        expect(e.client).toBe(ldapClient);
       }
     });
   });
 
   describe('when no error occured', () => {
-    const client = {
-      bind: (client_, b, cb) => {
+    const ldapClient = {
+      bind: (client, dn, cb) => {
         cb();
       },
     };
 
     it('should have no error message', async () => {
-      const res = await bindToLDAP(client, 'dn', 'password');
+      const res = await bindToLDAP(ldapClient, 'dn', 'password');
 
       expect(res.err).toBe(null);
     });
 
     it('should return client', async () => {
-      const res = await bindToLDAP(client, 'dn', 'password');
+      const res = await bindToLDAP(ldapClient, 'dn', 'password');
 
-      expect(res.client).toBe(client);
+      expect(res.client).toBe(ldapClient);
     });
   });
 });
