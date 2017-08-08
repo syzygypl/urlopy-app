74677c9df5c266f01653aa242294f7fc30ad9260
bsk <bartlomiej.glowala@syzygy.pl>
2017-08-08 13:23:41 +0200

    Remove sensitive postmark data (API KEY moved to env vars)

--- srcServer/mail/MailSender.js
+++ srcServer/mail/MailSender.js
@@ -1,20 +1,20 @@
 const postmark = require('postmark');
 
-const client = new postmark.Client('f12fa17c-6ef7-4c25-846f-119977f6506a');
+const client = new postmark.Client(process.env.POSTMARK_API_KEY);
 
 const prepareVacationDataString = (dates, notes) => `
 ${dates.map(date => `${date.from} - ${date.to} (dni roboczych: ${date.workDays})\r\n`).join('')}\r\n
 ${notes}`;
 
 const sendVacationRequestNotification = (from, to, dates, notes) => client.sendEmail({
-  From: 'admin.mirek@szg.io',
+  From: from.mail,
   To: to.mail,
   Subject: `[urlop] ${from.name}`,
   TextBody: prepareVacationDataString(dates, notes),
 });
 
 const sendStatusNotification = (to, status, dates, notes) => client.sendEmail({
-  From: 'admin.mirek@szg.io',
+  From: process.env.POSTMARK_DEFAULT_SENDER_MAIL,
   To: to.mail,
   Subject: `[urlop] ${to.name} - ${status}`,
   TextBody: `Status urlopu: ${status}\r\n\r\n${prepareVacationDataString(dates, notes)}`,
